90252f3acd95bf1a4d7ed4e0006b16ce9ba9daa1
Said Khail <saidkhail@hotmail.com>
2024-02-26 10:13:22 +0000

    Updating the questions in the index.js file and fixing the errors

--- index.js
+++ index.js
@@ -1,15 +1,127 @@
+//Lib Modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+//Node Modules 
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./src/page-template.js");
 
+//An Array to store the answers whe asking the questions 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+const newStaff = [];
+
+//CLI Prompt
+// array of questions for user
+const questions = async () => {
+ const answers = await inquirer
+  .prompt([
+   {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    validate: function (value) {
+     if (value.trim() === '') {
+      return 'Please enter your name? ' // ensure a user type in the required field
+     }
+     return true;
+    }
+   },
+   {
+    type: 'input',
+    name: 'id',
+    message: 'What is your Id Number?',
+    validate: function (value) {
+     if (value.trim() === '') {
+      return 'Please enter your ID number?'
+     }
+     return true;
+    }
+   },
+   {
+    type: 'input',
+    name: 'email',
+    message: ' Provide your Email Address?',
+    validate: function (value) {
+     if (value.trim() === '') {
+      return 'Please add your Email Address?'
+     }
+     return true;
+    }
+   },
+   {
+    type: 'input',
+    name: 'role',
+    message: 'Provide your Role?',
+    validate: function (value) {
+     if (value.trim() === '') {
+      return 'Please Provide your Role? '
+     }
+     return true;
+    },
+
+   }]);
+
+
+ // if Engineer role is used and new Engineer
+ if (answers.roles === "Engineer") {
+  const githubNumber = await inquirer
+   .prompt({
+    type: 'input',
+    name: 'Github',
+    message: 'Please provide your Github userName?',
+    validate: function (value) {
+     if (value.trim() === '') {
+      return 'Please Provide the correct  Github userName?'
+     }
+     return true;
+    },
+   }),
+  const newEngineer = new Engineer(
+   answers.name,
+   answers.id,
+   answers.email,
+   githubNumber.github
+  );
+  newStaff.push(newEngineer);
+ }
+
+
+ //If Intern is used and new Intern 
+
+
+
+
+ // if Manager Role is used and new Manager
+ if (answers.roles === "Manager") {
+  const managerIdNumber = await inquirer
+   .prompt({
+    type: 'input',
+    name: 'officeNumber',
+    message: 'Please Provide your Office Number?',
+    validate: function (value) {
+     if (value.trim() === '') {
+      return 'Please Provide the correct Office Number?'
+     }
+     return true;
+    },
+   }),
+  const newManager = new Manager(
+   answers.name,
+   answers.id,
+   answers.email,
+   managerIdNumber.officeNumber
+  );
+  newStaff.push(newManager);
+ }
+
+
+
+
+
 
