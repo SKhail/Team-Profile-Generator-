02a277cdc4ec28e9953029679706bd79044f40ab
Said Khail <saidkhail@hotmail.com>
2024-02-26 14:31:24 +0000

    Fixed the errors with the methods

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Team-Profile-Generator-
+
+### Resources
+
+[https://www.npmjs.com/package/inquirer]
+[https://jestjs.io/docs/getting-started]
--- index.js
+++ index.js
@@ -11,160 +11,159 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
-const { default: generate } = require("@babel/generator");
+
 
 //An Array to store the answers whe asking the questions 
 
-const newStaff = [];
+const team = [];
 
 //CLI Prompt
 // array of questions for user
 const questions = async () => {
- const answers = await inquirer
-  .prompt([
-   {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
-    // ensure a user type in the required field
-    validate: function (value) {
-     if (value.trim() === '') {
-      return 'Please enter your name? '
-     }
-     return true;
-    }
-   },
-   {
-    type: 'input',
-    name: 'id',
-    message: 'What is your ID Number?',
-    //Validation consisting only digits 
-    validate: function (value) {
-     if (!/^\d+$/.text(value.trim())) {
-      return 'Please enter a valid ID containing digits only'
-     }
-     return true;
-    }
-   },
-   {
-    type: 'input',
-    name: 'email',
-    message: ' Provide your Email Address?',
-    // it meets the correct email address format
-    validate: function (value) {
-     const emailChar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  //Check if 
-     if (!emailChar.test(value)) {
-      return 'Enter a valid Email Address'
-     }
-     return true;
-    }
-   },
-   {
-    type: 'input',
-    name: 'role',
-    message: 'Provide your Role?',
-    // ensure a user type in the required field
-    validate: function (value) {
-     if (value.trim() === '') {
-      return 'Please Provide your Role? '
-     }
-     return true;
-    },
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        // ensure a user type in the required field
+        validate: function (value) {
+          if (value.trim() === '') {
+            return 'Please enter your name? '
+          }
+          return true;
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID Number?',
+        //Validation consisting only digits 
+        validate: function (value) {
+          if (!/^\d+$/.test(value.trim())) {
+            return 'Please enter a valid ID containing digits only'
+          }
+          return true;
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: ' Provide your Email Address?',
+        // it meets the correct email address format
+        validate: function (value) {
+          const emailChar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  //Check if 
+          if (!emailChar.test(value)) {
+            return 'Enter a valid Email Address'
+          }
+          return true;
+        }
+      },
+      {
+        type: 'input',
+        name: 'role',
+        message: 'Provide your Role?',
+        // ensure a user type in the required field
+        validate: function (value) {
+          if (value.trim() === '') {
+            return 'Please Provide your Role? '
+          }
+          return true;
+        },
 
-   }]);
+      }]);
 
- // if Engineer role is used 
- if (answers.role === "Engineer") {
-  // Prompt The user for the Github
-  const { github } = await inquirer.prompt({
-   type: 'input',
-   name: 'Github',
-   message: 'Please provide your Github userName?',
-   //Validation that the field is not empty
-   validate: function (value) {
-    if (value.trim() === '') {
-     return 'Please Provide the correct  Github userName?'
-    }
-    return true;
-   },
-  });
-  //Create new engineer instance and add it to the newstaff arr
-  const newEngineer = new Engineer(
-   answers.name,
-   answers.id,
-   answers.email,
-   github
-  );
-  newStaff.push(newEngineer);
- }
+  // if Engineer role is used 
+  if (answers.role === "Engineer") {
+    // Prompt The user for the Github
+    const { github } = await inquirer.prompt({
+      type: 'input',
+      name: 'github',
+      message: 'Please provide your Github userName?',
+      //Validation that the field is not empty
+      validate: function (value) {
+        if (value.trim() === '') {
+          return 'Please Provide the correct  Github userName?'
+        }
+        return true;
+      },
+    });
+    //Create new engineer instance and add it to the team arr
+    const newEngineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      github
+    );
+    team.push(newEngineer);
+  }
 
- //If Intern is used 
- if (answers.role === "Intern") {
-  // Prompt The user for the name of University
-  const { school } = await inquirer
-   .prompt({
-    type: 'input',
-    name: 'school',
-    message: 'Please provide the University you attend?',
-    //Validation that the field is not empty
-    validate: function (value) {
-     if (value.trim() === '') {
-      return 'Please Provide the correct University you attend?'
-     }
-     return true;
-    },
-   });
-  //Create new Intern instance and add it to the newstaff arr
-  const newIntern = new Intern(
-   answers.name,
-   answers.id,
-   answers.email,
-   school
-  );
-  newStaff.push(newIntern);
- }
+  //If Intern is used 
+  if (answers.role === "Intern") {
+    // Prompt The user for the name of University
+    const { school } = await inquirer
+      .prompt({
+        type: 'input',
+        name: 'school',
+        message: 'Please provide the University you attend?',
+        //Validation that the field is not empty
+        validate: function (value) {
+          if (value.trim() === '') {
+            return 'Please Provide the correct University you attend?'
+          }
+          return true;
+        },
+      });
+    //Create new Intern instance and add it to the team arr
+    const newIntern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      school
+    );
+    team.push(newIntern);
+  }
 
 
- // if Manager Role is used 
- if (answers.role === "Manager") {
-  // Prompt The user for the their Office Number
-  const { officeNumber } = await inquirer
-   .prompt({
-    type: 'input',
-    name: 'officeNumber',
-    message: 'Please Provide your Office Number?',
-    //that the field contains Digits only
-    validate: function (value) {
-     if (!/^\d+$/.text(value.trim())) {
-      return 'Please enter a valid ID containing digits only'
-     }
-     return true;
-    }
-   });
-  //Create new Manager instance and add it to the newstaff arr
-  const newManager = new Manager(
-   answers.name,
-   answers.id,
-   answers.email,
-   officeNumber
-  );
-  newStaff.push(newManager);
- }
+  // if Manager Role is used 
+  if (answers.role === "Manager") {
+    // Prompt The user for the their Office Number
+    const { officeNumber } = await inquirer
+      .prompt({
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please Provide your Office Number?',
+        //that the field contains Digits only
+        validate: function (value) {
+          if (!/^\d+$/.test(value.trim())) {
+            return 'Please enter a valid ID containing digits only'
+          }
+          return true;
+        }
+      });
+    //Create new Manager instance and add it to the team arr
+    const newManager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      officeNumber
+    );
+    team.push(newManager);
+  }
 
 
 }
 
 
-function buildTeam() {
- console.log("new person", newStaff)
- fs.writeFileSync("./index.html",
-  render(newStaff), "utf-8");
+async function buildTeam() {
+  //prompting the user the questions first
+  await questions();
+  console.log("new person", team)
+  fs.writeFileSync("./index.html",
+    render(team), "utf-8");
 
 }
 
 buildTeam();
-questions();
-
-
 
 
